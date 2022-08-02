492094d91db3f9aa715a9a231b9547c989670187
bissu <bismeetsingh352@gmail.com>
2022-08-02 15:24:04 +0530

    Created grid based UI

--- js/navbar.js
+++ js/navbar.js
@@ -2,11 +2,11 @@ var sliderIndex = 0;
 
 const toggleMenuMobile = () => {
     const header  = document.getElementsByClassName('header')[0];
-    if (header.style.transform ===  'translateX(-100%)'){
+    if (header.style.transform ===  'translateX(-1000px)'){
         header.style.transform = 'translateX(0%)';  
     }
     else{
-        header.style.transform = 'translateX(-100%)';       
+        header.style.transform = 'translateX(-1000px)';       
     
     }
   
